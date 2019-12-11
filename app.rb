c0f03010a44cf35155a0bb703374fbb3a1bc278a
Lope Ariyo <lopeariyo@gmail.com>
2019-12-11 18:02:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,24 @@ module FormsLab
 
     # code other routes/actions here
 
+    get ('/') {
+      erb :root
+    }
+
+    get ('/new') {
+      erb :"pirates/new"
+    }
+
+    post ('/pirates'){
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each { |info|
+        Ship.new(info)
+      } 
+
+      @ships = Ship.all
+
+      erb :"pirates/show"
+     }
   end
 end
